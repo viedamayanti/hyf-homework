e749b17c3894af5a5a1934aa573a7a86f957b26c
viedamayanti <103531391+viedamayanti@users.noreply.github.com>
2022-08-29 10:10:00 +0200

    Update hyf-week1.sql

--- databases/week1/hyf-week1.sql
+++ databases/week1/hyf-week1.sql
@@ -1,51 +1,45 @@
 USE `hyf-week1`;
- 
-SELECT * FROM `task`;
-SELECT * FROM `user`;
-SELECT * FROM `status`;
-SELECT `status`. `name` FROM `task`;
-SELECT * FROM `task` WHERE status_id != 3;
--- Find out how many tasks are in the task table
-SELECT COUNT(id) FROM `task`;
+-- 1
+SELECT COUNT(*) FROM task;
+-- 2
+SELECT * FROM task
 
--- Find out how many tasks in the task table do not have a valid due date
-SELECT * FROM `task`WHERE due_date IS NULL;
+-- 3
+WHERE due_date IS NULL;
+-- 4
+SELECT * FROM task 
+JOIN status ON task.status_id = status_id
+WHERE status.name = 'Done';
 
--- Find all the tasks that are marked as done
-SELECT * FROM `task` WHERE status_id = 3;
+-- 5
+SELECT * FROM task
+ORDER BY due_date DESC;
 
--- Find all the tasks that are not marked as done
-SELECT * FROM `task` WHERE status_id < 3;
+-- 6
+SELECT * FROM task
+ORDER BY due_date DESC
+LIMIT 1;
 
--- Get all the tasks, sorted with the most recently created first
-SELECT * FROM `task` ORDER BY due_date DESC;
+-- 7
+SELECT title, due_date FROM task
+WHERE title REGEXP 'database' OR description REGEXP 'database';
 
--- Get the single most recently created task
-SELECT * FROM `task` ORDER BY due_date DESC LIMIT 1;
+-- 8
+SELECT title, status_id AS 'text' FROM task
+JOIN status ON status_id = task.status_id
+ORDER BY status.name;
 
--- Get the title and due date of all tasks where the title or description contains database
-SELECT `title`, `due_date`
-FROM `task`
-WHERE `title` 
-LIKE '%database%' 
-OR `description` 
-LIKE '%database%';
-
--- Get the title and status (as text) of all tasks
-SELECT title, status_id AS 'text'
-FROM `task`;
-
--- Get the name of each status, along with a count of how many tasks have that status
-SELECT status.name, 
-COUNT(task.id)
-FROM `task`
-JOIN `status`
+-- 9
+SELECT status.name, COUNT(task.id)
+FROM task
+JOIN status
 ON task.status_id = status.id
 GROUP BY status.name;
 
--- Get the names of all statuses, sorted by the status with most tasks first
-SELECT MAX(task.id)
-FROM `task`
-JOIN `status`
-ON task.status_id = status.id;
-
+-- 10
+SELECT status.name, COUNT(task.id)
+FROM task
+JOIN status
+ON task.status_id = status.id
+GROUP BY status.name
+ORDER BY COUNT(task.id) DESC; 
