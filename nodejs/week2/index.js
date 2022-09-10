9548da3bc4572c19402216c720b517d4a5f3fa38
viedamayanti <vinaniekdamayanti@gmail.com>
2022-09-10 15:43:58 +0200

    updated nodejs-week2

--- nodejs/week2/index.js
+++ nodejs/week2/index.js
@@ -1,6 +1,7 @@
+const { Router } = require('express');
 const express = require('express');
 const app = express();
-
+const router = express.Router();
 app.get('/', (req, res) => res.send('nodejs week2 homework'));
 
 app.get('/numbers/add', (req, res) => {
@@ -12,5 +13,5 @@ app.get('/numbers/multiply/:first/:second', (req, res) => {
   console.log(req.params);
   res.send(String(Number(req.params.first) * Number(req.params.second)));
 });
-
+app.use('/about', router);
 app.listen(3000, () => console.log(`Calculator:listening on port 3000`));
