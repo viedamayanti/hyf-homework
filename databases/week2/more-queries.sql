43b76e7775a9771668d94e2692c1954ffa01e925
viedamayanti <vinaniekdamayanti@gmail.com>
2022-09-10 17:38:43 +0200

    updated

--- databases/week2/more-queries.sql
+++ databases/week2/more-queries.sql
@@ -2,16 +2,17 @@ USE hyf;
 select * from task;
 
 -- 1 
-SELECT  user.name, user.email FROM user
-WHERE user.email REGEXP '@spotify.com$';
+select * from task
+join user
+where task.id = user.email REGEXP '@spotify.com$';
 
 -- 2
-SELECT * FROM task
-JOIN user 
-ON task.id = user.id
-JOIN status
-ON task.id = task.status_id
-WHERE user.name REGEXP 'Donald Duck' AND status.name = 'Not started' ;
+select * from task
+join user
+on task.id = user.id
+join status
+on task.status_id = status.id
+where user.name = 'Donald Duck' and status.name = 'Not started';
 
 -- 3
 SELECT * FROM task
@@ -20,5 +21,5 @@ ON task.id = user.id
 WHERE user.name REGEXP 'Maryrose Meadows' AND month(created) = 09 ;
 
 -- 4
-SELECT COUNT(*) as october FROM task
-GROUP BY month(created) = 10;
+select count(*) as task from task 
+group by month(created);
