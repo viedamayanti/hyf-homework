ef8a85a70bd4f2d2a4004c8648ca6eee85384bfa
viedamayanti <vinaniekdamayanti@gmail.com>
2022-09-20 21:03:47 +0200

    nodejs-week3

---
+++ nodejs/week3/app.js
@@ -0,0 +1,67 @@
+// @ts-ignore
+const knex = require('knex')({
+  client: 'mysql2',
+  connection: {
+    host: process.env.DB_HOST || '0.0.0.0',
+    port: process.env.DB_PORT || 33060,
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'pass',
+    database: process.env.DB_NAME || 'hyf_node_week3_warmup',
+    multipleStatements: true,
+  },
+});
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+const apiRouter = express.Router();
+app.use('/api', apiRouter);
+
+const contactsAPIRouter = express.Router();
+apiRouter.use('/contacts', contactsAPIRouter);
+
+contactsAPIRouter.get('/', async (req, res) => {
+  let query = knex.select('').from('contacts');
+
+  if ('sort' in req.query) {
+    // @ts-ignore
+    const orderBy = req.query.sort.toString();
+    if (orderBy.length > 0) {
+      query = query.orderByRaw(orderBy);
+    }
+  }
+
+  console.log('SQL', query.toSQL().sql);
+
+  try {
+    const data = await query;
+    res.json({ data });
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+contactsAPIRouter.get('/', async (req, res) => {
+  try {
+    const result = await knex.orderBy('first_name', 'asc');
+    res.send(req.query.sort);
+  } catch (error) {
+    res.status(500).json({ Msg: 'Internal server error' });
+  }
+});
+contactsAPIRouter.get('/', async (req, res) => {
+  try {
+    const result = await knex.orderBy('last_name', 'desc');
+    res.send(req.query.sort);
+  } catch (error) {
+    res.status(500).json({ Msg: 'Internal server error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
