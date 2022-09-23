a9246c70f7622de54443f37725c2c664d4d845e1
viedamayanti <vinaniekdamayanti@gmail.com>
2022-09-23 08:14:21 +0200

    nodejs-week2

--- nodejs/week2/app.js
+++ nodejs/week2/app.js
@@ -10,48 +10,40 @@ app.get('/', (req, res) => {
   res.send('This is a search engine');
 });
 
+// It does not work
 app.get('/search', (req, res) => {
   console.log(req.query);
-  if (req.query.q) {
-    return values;
+  const queryValue = req.query.q;
+  if (queryValue) {
+    const values = doc.filter((item) => {
+      return Object.values(item)
+        .map(String)
+        .some((value) =>
+          // @ts-ignore
+          value.toLowerCase().includes(queryValue.toLowerCase())
+        );
+    });
+    return res.json(values);
   }
   return res.json(doc);
-});
-
-app.get('/documents/:id', (req, res) => {
-  const found = doc.some((document) => document.id === parseInt(req.params.id));
-  if (found) {
-    res.json(doc.filter((document) => document.id === parseInt(req.params.id)));
-  } else {
-    res.status(404).json(`The document ${req.params.id} is not found`);
-  }
 });
 
 app.post('/search', (req, res) => {
-  if (req.body.fields) {
-    if (req.query.q) {
-      return res.send({ msg: 'This is an error message' }).status(400);
-    }
-
-    const filtered = doc.filter((item) => {
-      const keyName = Object.keys(req.body.fields)[0];
-      const value = Object.values(req.body.fields)[0];
-
-      return item[keyName].includes(value);
-    });
-    return res.json(filtered);
-  }
-
-  return res.json(doc);
+  console.log(req.body.fields);
+  res.json(doc);
 });
 
-let values = Object.values(
-  doc.filter((item) => {
-    return String(item).includes('Mobile');
-  })
-);
+app.get('/documents/:id', (req, res) => {
+  const filtered = doc.some(
+    (document) => document.id === parseInt(req.params.id)
+  );
+  if (filtered) {
+    res.json(doc.filter((document) => document.id === parseInt(req.params.id)));
+  } else {
+    res.status(404).send(`The document ${req.params.id} is not found`);
+  }
+});
 
-console.log(values);
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
