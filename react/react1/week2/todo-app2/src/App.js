5b88841f3119734f3f0d1710ee691602dcb49e34
viedamayanti <vinaniekdamayanti@gmail.com>
2022-10-20 19:26:38 +0200

    Fix after review

--- react/react1/week2/todo-app2/src/App.js
+++ react/react1/week2/todo-app2/src/App.js
@@ -2,25 +2,8 @@ import React, { useState } from 'react';
 import Header from './components/Header';
 import Timer from './components/Timer';
 import TodoLists from './components/TodoLists';
+import todoItems from './components/TodoItems';
 import './App.css';
-
-const todoItems = [
-  {
-    id: 1,
-    activity: 'Get out of bed',
-    dateline: 'Wed Sep 13 2017',
-  },
-  {
-    id: 2,
-    activity: 'Brush teeth',
-    dateline: 'Thu Sep 14 2017',
-  },
-  {
-    id: 3,
-    activity: 'Eat breakfast',
-    dateline: 'Fri Sep 15 2017',
-  },
-];
 
 function App() {
   const [todos, setTodos] = useState(todoItems);
@@ -31,10 +14,7 @@ function App() {
       activity: 'Random text',
       dateline: 'Wed Sep 13 2017',
     };
-
-    const mynextTodo = todos.concat(newTodo);
     setTodos([...todos, newTodo]);
-    console.log('My next todo is', mynextTodo);
   };
 
   const onDelete = (id) => {
@@ -50,7 +30,7 @@ function App() {
       {todos.length === 0 && <h1>No Item</h1>}
       <p>
         {todos.map((item) => (
-          <TodoLists item={item} onDelete={onDelete} />
+          <TodoLists key={item.id} item={item} onDelete={onDelete} />
         ))}
       </p>
 
---
+++ react/react1/week2/todo-app2/src/components/TodoItems.js
@@ -0,0 +1,19 @@
+const todoItems = [
+  {
+    id: 1,
+    activity: 'Get out of bed',
+    dateline: 'Wed Sep 13 2017',
+  },
+  {
+    id: 2,
+    activity: 'Brush teeth',
+    dateline: 'Thu Sep 14 2017',
+  },
+  {
+    id: 3,
+    activity: 'Eat breakfast',
+    dateline: 'Fri Sep 15 2017',
+  },
+];
+
+export default todoItems;
